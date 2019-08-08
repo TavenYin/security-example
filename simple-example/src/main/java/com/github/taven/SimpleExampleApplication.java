e796bbad61cf6fe0cea8337d51f122d05ef2a7ae
yintianwen <yintianwen7@qq.com>
2019-08-08 11:18:49 +0800

    simple example

---
+++ simple-example/src/main/java/com/github/taven/SimpleExampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.taven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleExampleApplication.class, args);
+	}
+
+}
